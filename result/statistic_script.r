4b5383251e814494b6570c0a35543c5fad2f8089
Martin Wigger <martin.wigger@students.unibe.ch>
2017-11-07 11:38:47 +0100

    Add new Messurements

--- result/statistic_script.r
+++ result/statistic_script.r
@@ -6,13 +6,21 @@ setwd("/home/martin/CloudStation/Schule/Msc/3Semester/Medical_Image_Analysis_Lab
 df1 <- read.csv("results-reference.csv", header = TRUE, sep = ";")
 df2 <- read.csv("results-zcore.csv", header = TRUE, sep = ";")
 df3 <- read.csv("results-medianR1.csv", header = TRUE, sep = ";")
-df4 <- read.csv("results-medianR1_Zscore", header = TRUE, sep = ";")
+df4 <- read.csv("results-medianR2.csv", header = TRUE, sep = ";")
+df5 <- read.csv("results-medianR3.csv", header = TRUE, sep = ";")
+df6 <- read.csv("results-medianR3_Zscore.csv", header = TRUE, sep = ";")
+df7 <- read.csv("results-medianR1_Zscore.csv", header = TRUE, sep = ";")
+
 df1["ALgorithm"] <- "0 Ref"
 df2["ALgorithm"] <- "ZScore"
 df3["ALgorithm"] <- "MedianR1"
-df4["ALgorithm"] <- "MedianR1-Zscore"
+df4["ALgorithm"] <- "MedianR2"
+df5["ALgorithm"] <- "MedianR3"
+df6["ALgorithm"] <- "MedianR3-Zscore"
+df7["ALgorithm"] <- "MedianR1-Zscore"
 
-total <- rbind(df1,df2,df3,df4)
+
+total <- rbind(df1,df2,df3,df4,df5,df6,df7)
 
 #colnames(df1)[colnames(df1)=="DICE"] <- "DICE_REF"
 #colnames(df2)[colnames(df2)=="DICE"] <- "DICE_ZSCORE"
@@ -29,17 +37,20 @@ by(dat_merge, dat_merge$LABEL, summary)
 
 spt1 <- split(total, total$LABEL) 
 
-png('boxplot_Ventricles.png')
+#png('boxplot_Ventricles.png')
+postscript('boxplot_Ventricles.ps')
 boxplot(DICE ~ ALgorithm,data=spt1[["Ventricles"]],main="Ventricles")
 grid(nx=NA, ny=NULL) #grid over boxplot
 dev.off()
 
-png('boxplot_GreyMatter.png')
+png('boxplot_GreyMatter.png', width = 800, height =500 , units = 'px')
+#postscript('boxplot_GreyMatter.ps')
 boxplot(DICE ~ ALgorithm,data=spt1[["GreyMatter"]],main="GreyMatter")
 grid(nx=NA, ny=NULL) #grid over boxplot
 dev.off()
 
-png('boxplot_WhithMatter.png')
+#png('boxplot_WhithMatter.png')
+postscript('boxplot_WhithMatter.ps')
 boxplot(DICE ~ ALgorithm,data=spt1[["WhiteMatter"]],main="WhiteMatter")
 grid(nx=NA, ny=NULL) #grid over boxplot
 dev.off()
