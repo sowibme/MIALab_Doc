3138db32257791d3d3afb6e612cc4c4581047eb1
Martin Wigger <martin.wigger@students.unibe.ch>
2017-12-18 07:37:24 +0100

    Add Bilateral Filter

--- result/statistic_script.r
+++ result/statistic_script.r
@@ -14,20 +14,26 @@ df8 <- read.csv("results-Zscore_medianR2.csv", header = TRUE, sep = ";")
 df9 <- read.csv("results_mni_biasfieldcorr.csv", header = TRUE, sep = ";")
 df10 <- read.csv("results_nmi.csv", header = TRUE, sep = ";")
 df11 <- read.csv("results_gaussian_S1.csv", header = TRUE, sep = ";")
+df12 <- read.csv("results_gaussian_S3.csv", header = TRUE, sep = ";")
+df13 <- read.csv("results_bilateral_ds4_rs50_with_skull.csv", header = TRUE, sep = ";")
+df14 <- read.csv("results_bilateral_ds4_rs50.csv", header = TRUE, sep = ";")
 
-df1["ALgorithm"] <-  "02 Ref"
-df2["ALgorithm"] <-  "07 ZScore"
+df1["ALgorithm"] <-  "00 Ref"
+df2["ALgorithm"] <-  "02 ZScore"
 df3["ALgorithm"] <-  "01 MedianR1"
-df4["ALgorithm"] <-  "02 MedianR2"
-df5["ALgorithm"] <-  "03 MedianR3"
-df6["ALgorithm"] <-  "05 MedianR3-Zscore"
-df7["ALgorithm"] <-  "04 MedianR1-Zscore"
-df8["ALgorithm"] <-  "08 Zscore-MedianR2"
+df4["ALgorithm"] <-  "01 MedianR2"
+df5["ALgorithm"] <-  "01 MedianR3"
+df6["ALgorithm"] <-  "03 MedianR3-Zscore"
+df7["ALgorithm"] <-  "03 MedianR1-Zscore"
+df8["ALgorithm"] <-  "04 Zscore-MedianR2"
 df9["ALgorithm"] <-  "01 only biasfieldcorr"
 df10["ALgorithm"] <- "00 raw"
-df11["ALgorithm"] <- "09 GaussianS1"
+df11["ALgorithm"] <- "05 GaussianS1"
+df12["ALgorithm"] <- "05 GaussianS3"
+df13["ALgorithm"] <- "09 Bilateral_skull"
+df14["ALgorithm"] <- "09 Bilateral"
 
-total <- rbind(df1,df2,df3,df4,df5,df6,df7,df8,df9,df10,df11)
+total <- rbind(df1,df2,df3,df4,df5,df6,df7,df8,df9,df10,df11,df12,df13,df14)
 
 #colnames(df1)[colnames(df1)=="DICE"] <- "DICE_REF"
 #colnames(df2)[colnames(df2)=="DICE"] <- "DICE_ZSCORE"
